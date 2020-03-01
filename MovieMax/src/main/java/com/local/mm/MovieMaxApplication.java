656e42b05b6b51436894256db9adaf8cd24bbff8
Rekhit <officialrek.07@gmail.com>
2020-03-01 19:07:16 +0530

    First Commit

---
+++ MovieMax/src/main/java/com/local/mm/MovieMaxApplication.java
@@ -0,0 +1,13 @@
+package com.local.mm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMaxApplication.class, args);
+	}
+
+}
