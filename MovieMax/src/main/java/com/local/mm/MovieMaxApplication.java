5ff365bf0b6ce6be0dae250e4ecaf231e634a693
Rekhit <officialrek.07@gmail.com>
2020-03-01 19:31:39 +0530

    Second Commit

--- MovieMax/src/main/java/com/local/mm/MovieMaxApplication.java
+++ MovieMax/src/main/java/com/local/mm/MovieMaxApplication.java
@@ -1,13 +1,20 @@
 package com.local.mm;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MovieMaxApplication {
+public class MovieMaxApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieMaxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
